e4a7fa34f4ce98ff8af26a526ef2354f3d235617
Achim Kraus <achim.kraus@bosch-si.com>
2019-01-11 15:38:02 +0100

    Add ciphersuites for interop tests.
    TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256 (0xc02b) and
    TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA (0xc00a).
    Add oppenssl script to ensure compatibility of new cipher suite.
    
    Signed-off-by: Achim Kraus <achim.kraus@bosch-si.com>

--- scandium-core/src/test/java/org/eclipse/californium/scandium/config/DtlsConnectorConfigTest.java
+++ scandium-core/src/test/java/org/eclipse/californium/scandium/config/DtlsConnectorConfigTest.java
@@ -122,7 +122,8 @@ public class DtlsConnectorConfigTest {
 		List<CipherSuite> cipherSuites = config.getSupportedCipherSuites();
 		assertThat(cipherSuites, ListUtilsTest.containsAll(CipherSuite.TLS_PSK_WITH_AES_128_CBC_SHA256,
 				CipherSuite.TLS_PSK_WITH_AES_128_CCM_8, CipherSuite.TLS_ECDHE_PSK_WITH_AES_128_CBC_SHA256,
-				CipherSuite.TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA256, CipherSuite.TLS_ECDHE_ECDSA_WITH_AES_128_CCM_8));
+				CipherSuite.TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA, CipherSuite.TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA384,
+				CipherSuite.TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA256, CipherSuite.TLS_ECDHE_ECDSA_WITH_AES_128_CCM_8, CipherSuite.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256));
 	}
 
 	@Test(expected = IllegalStateException.class)
